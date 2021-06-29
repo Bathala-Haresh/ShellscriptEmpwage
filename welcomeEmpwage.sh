0aa56926af358a583a7920cdeb689bbfd35354c4
Bathala-Haresh <hareshbathala87@gmail.com>
2021-06-29 19:54:00 +0530

    emp wage program

--- welcomeEmpwage.sh
+++ welcomeEmpwage.sh
@@ -1,2 +1,13 @@
+#INITIAL VALUE TO ASSING PRESENT
+isPresent=1
 
-echo "-------------------------------------------- Welcome To Employee Wage Compution -------------------------------------------------------"
+#TO VARIABLE
+randomNumber=$((RANDOM%2))
+
+#TO CHECK THE EMPLOYEE PRESENT AND ABSENT
+if [ $randomNumber -eq 	$isPresent ]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
